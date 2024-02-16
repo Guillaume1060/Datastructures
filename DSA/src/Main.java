80ec0fdc6e4aa2ccbc37710e21bb40218db9f1ef
Guillaume <breyerguillaume@gmail.com>
2024-02-16 12:10:56 +0100

    LinkedList_basicExercises

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(0);
-        myLinkedList.append(1);
-        myLinkedList.append(2);
+        LinkedList myLinkedList = new LinkedList(11);
         myLinkedList.append(3);
+        myLinkedList.append(23);
+        myLinkedList.append(7);
 
 //        myLinkedList.prepend(1);
-        System.out.println(myLinkedList.get(2).value);
+//        System.out.println(myLinkedList.get(2).value);
+//        myLinkedList.printList();
+        myLinkedList.set(1,4);
+        myLinkedList.insert(0,99);
+        myLinkedList.insert(1,98);
         myLinkedList.printList();
 /*        System.out.println(myLinkedList.removeLast().value);
         System.out.println(myLinkedList.removeLast().value);
