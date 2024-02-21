b42c41c282f672ceaccb3c48edbc8791479a80f0
Guillaume <breyerguillaume@gmail.com>
2024-02-21 10:12:38 +0100

    DoublyLinkedList Constructor

--- DSA/src/LL_LESSONS/Main.java
+++ DSA/src/LL_LESSONS/Main.java
@@ -2,21 +2,29 @@ package LL_LESSONS;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(11);
-        myLinkedList.append(3);
-        myLinkedList.append(23);
-        myLinkedList.append(7);
+//        LinkedList myLinkedList = new LinkedList(11);
+//        myLinkedList.append(3);
+//        myLinkedList.append(23);
+//        myLinkedList.append(7);
 
 //        myLinkedList.prepend(1);
 //        System.out.println(myLinkedList.get(2).value);
 //        myLinkedList.printList();
-        myLinkedList.set(1,4);
-        myLinkedList.insert(0,99);
-        myLinkedList.insert(1,98);
-        myLinkedList.printList();
+//        myLinkedList.set(1,4);
+//        myLinkedList.insert(0,99);
+//        myLinkedList.insert(1,98);
+//        myLinkedList.printList();
 /*        System.out.println(myLinkedList.removeLast().value);
         System.out.println(myLinkedList.removeLast().value);
         System.out.println(myLinkedList.removeLast());*/
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.printList();
 
     }
 }
