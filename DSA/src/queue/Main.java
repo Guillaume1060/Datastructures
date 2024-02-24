3b0d51b11a2759252dff4d181ca9099955b8e893
Guillaume <breyerguillaume@gmail.com>
2024-02-24 18:01:14 +0100

    Queue : enqueue method

--- DSA/src/queue/Main.java
+++ DSA/src/queue/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Queue myQueue = new Queue(5);
 
+        myQueue.enqueue(12);
         myQueue.getFirst();
         myQueue.getLast();
         myQueue.getLength();
