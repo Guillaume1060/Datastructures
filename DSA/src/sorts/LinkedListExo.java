b232303db69f70f288baa1ee184927f75cb23cd3
Guillaume <breyerguillaume@gmail.com>
2024-03-09 17:51:22 +0100

    Sorting

--- DSA/src/sorts/LinkedListExo.java
+++ DSA/src/sorts/LinkedListExo.java
@@ -62,12 +62,12 @@ public class LinkedListExo {
 
     //EXO 89
     // WRITE BUBBLESORT METHOD HERE //
-    public  void bubbleSort() {
+    public void bubbleSort() {
         if (length < 2) return;
         Node sortedUntil = null;
         while (sortedUntil != head.next) {
             Node currentNode = head;
-            while (currentNode.next!=sortedUntil) {
+            while (currentNode.next != sortedUntil) {
                 Node nextNode = currentNode.next;
                 if (currentNode.value > nextNode.value) {
                     int temp = currentNode.value;
@@ -78,6 +78,53 @@ public class LinkedListExo {
             }
             sortedUntil = currentNode;
         }
+    }
+
+    //EXO 90
+    public void selectionSort() {
+        if (length < 2) return;
+        Node current = head;
+        while (current.next != null) {
+            Node minVal = current;
+            Node innerCurrent = current.next;
+            while (innerCurrent != null) {
+                minVal = innerCurrent.value < minVal.value ? innerCurrent : minVal;
+                innerCurrent = innerCurrent.next;
+            }
+            if (current != minVal) {
+                int temp = current.value;
+                current.value = minVal.value;
+                minVal.value = temp;
+            }
+            current = current.next;
+
+        }
+        tail = current;
+    }
+
+    //EXO 91
+    public void insertionSort() {
+        if (length < 2) return;
+        Node sortedListHead = head;
+        Node unsortedListHead = head.next;
+        sortedListHead.next = null;
+
+        while (unsortedListHead != null) {
+            Node current = unsortedListHead;
+            unsortedListHead = unsortedListHead.next;
+            if (current.value<sortedListHead.value) {
+                current.next = sortedListHead;
+                sortedListHead = current;
+            } else {
+                Node searchPointer = sortedListHead;
+                while (searchPointer.next!=null && current.value>searchPointer.next.value) {
+                    searchPointer = searchPointer.next;
+                }
+                current.next = searchPointer.next;
+                searchPointer.next = current;
+            }
+        }
+        head = sortedListHead;
     }
 
 }
